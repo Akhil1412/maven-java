a713390f690c1513c1b0ff425c823ee91fb47c4d
Akhil1412 <modhalaakhil@gmail.com>
2023-11-24 10:38:22 +0530

    first commit

---
+++ src/main/java/maven_java/java/App.java
@@ -0,0 +1,13 @@
+package maven_java.java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
